2caa76706e8a64f5cf74ebc806d8557d7ed81cf7
doblerone <andreasd@met.no>
2022-06-20 10:56:00 +0000

    CNRM_ALADIN added.

--- prepare_input.sh
+++ prepare_input.sh
@@ -13,7 +13,7 @@ cd ${BASEDIR}
 for var in pr tas tasmin tasmax sfcWind hurs ps rlds rsds
  do mkdir -p ${BASEDIR}/${var}/Cur
  cd ${BASEDIR}/${var}/Cur
- for model in mpi-r1i1p1-cclm #cnrm-r1i1p1-racmo mpi-r2i1p1-remo ecearth-r12i1p1-cclm  ecearth-r3i1p1-hirham  noresm-r1i1p1-remo
+ for model in cnrm-r1i1p1-aladin #mpi-r1i1p1-cclm  cnrm-r1i1p1-racmo  mpi-r2i1p1-remo ecearth-r12i1p1-cclm  ecearth-r3i1p1-hirham  noresm-r1i1p1-remo
   do rsync -avu /lustre/storeC-ext/users/kin2100/NVE/EQM/${model}/${var}/hist/* .
  done
 done
@@ -25,7 +25,7 @@ cd ${BASEDIR}
 for var in pr tas tasmin tasmax sfcWind hurs ps rlds rsds
  do mkdir -p ${BASEDIR}/${var}/Fut
  cd ${BASEDIR}/${var}/Fut
- for model in mpi-r1i1p1-cclm #cnrm-r1i1p1-racmo mpi-r2i1p1-remo ecearth-r12i1p1-cclm  ecearth-r3i1p1-hirham  noresm-r1i1p1-remo
+ for model in cnrm-r1i1p1-aladin #mpi-r1i1p1-cclm  cnrm-r1i1p1-racmo  mpi-r2i1p1-remo ecearth-r12i1p1-cclm  ecearth-r3i1p1-hirham  noresm-r1i1p1-remo
   do rsync -avu /lustre/storeC-ext/users/kin2100/NVE/EQM/${model}/${var}/rcp??/* .
  done
 done
