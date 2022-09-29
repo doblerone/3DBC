524cae0b4fb071d8fc245a623b40c10f6f78aa3b
doblerone <andreasd@met.no>
2022-09-29 07:44:02 +0000

    Add new model

--- prepare_output.sh
+++ prepare_output.sh
@@ -14,8 +14,8 @@ cd ${BASEDIR}
 for var in pr tas tasmin tasmax sfcWind hurs ps rlds rsds
  do mkdir -p ${BASEDIR}/${var}/CurC/app
  cd ${BASEDIR}/${var}/CurC/app
- for model in hadgem-r1i1p1-remo hadgem-r1i1p1-rca #noresm-r1i1p1-rca cnrm-r1i1p1-aladin mpi-r1i1p1-cclm  cnrm-r1i1p1-racmo  mpi-r2i1p1-remo ecearth-r12i1p1-cclm  ecearth-r3i1p1-hirham  noresm-r1i1p1-remo
-  do rsync -avu ${BASEDIR}/${var}/Cur/*${model}*nc4 . #/lustre/storeC-ext/users/kin2100/NVE/EQM/${model}/${var}/hist/* .
+ for model in ecearth-r12i1p1-rca #hadgem-r1i1p1-remo hadgem-r1i1p1-rca noresm-r1i1p1-rca cnrm-r1i1p1-aladin mpi-r1i1p1-cclm  cnrm-r1i1p1-racmo  mpi-r2i1p1-remo ecearth-r12i1p1-cclm  ecearth-r3i1p1-hirham  noresm-r1i1p1-remo
+  do rsync -avu /lustre/storeC-ext/users/kin2100/NVE/EQM/${model}/${var}/hist/* . #${BASEDIR}/${var}/Cur/*${model}*nc4 .
  done
  #Rename files
  rename s/_eqm/_3dbc-eqm/ *nc4 #ubuntu & CentOS
@@ -28,8 +28,8 @@ done
 for var in pr tas tasmin tasmax sfcWind hurs ps rlds rsds
  do mkdir -p ${BASEDIR}/${var}/FutC
  cd ${BASEDIR}/${var}/FutC
- for model in hadgem-r1i1p1-remo hadgem-r1i1p1-rca #noresm-r1i1p1-rca cnrm-r1i1p1-aladin mpi-r1i1p1-cclm  cnrm-r1i1p1-racmo  mpi-r2i1p1-remo ecearth-r12i1p1-cclm  ecearth-r3i1p1-hirham  noresm-r1i1p1-remo
-  do rsync -avu ${BASEDIR}/${var}/Fut/*${model}*nc4 .  #/lustre/storeC-ext/users/kin2100/NVE/EQM/${model}/${var}/rcp??/* .
+ for model in ecearth-r12i1p1-rca #hadgem-r1i1p1-remo hadgem-r1i1p1-rca noresm-r1i1p1-rca cnrm-r1i1p1-aladin mpi-r1i1p1-cclm  cnrm-r1i1p1-racmo  mpi-r2i1p1-remo ecearth-r12i1p1-cclm  ecearth-r3i1p1-hirham  noresm-r1i1p1-remo
+  do rsync -avu /lustre/storeC-ext/users/kin2100/NVE/EQM/${model}/${var}/rcp??/* . #${BASEDIR}/${var}/Fut/*${model}*nc4 . 
  done
  #Rename files
  rename s/_eqm/_3dbc-eqm/ *nc4 #ubuntu & CentOS
